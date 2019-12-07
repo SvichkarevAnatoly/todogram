37bef65d259ef8132d25b7d20970c35a01aa4f9a
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2019-12-07 21:42:27 +0300

    вынос ключа для сервиса гифок

--- src/main/java/com/example/HelloBot.java
+++ src/main/java/com/example/HelloBot.java
@@ -28,9 +28,10 @@ public class HelloBot extends AbilityBot {
 
     private static int gifOffset = 0;
     private static final String queryWelcomeInRussian = "%D0%B4%D0%BE%D0%B1%D1%80%D0%BE%20%D0%BF%D0%BE%D0%B6%D0%B0%D0%BB%D0%BE%D0%B2%D0%B0%D1%82%D1%8C";
-    private static final String requestUrl = "https://api.giphy.com/v1/gifs/search?" +
-            "api_key=MDMQLQKA92JhXaGIbo2QFFZs5K4FdfD3&q=" +
-            queryWelcomeInRussian + "&limit=1&rating=G&lang=ru&offset=";
+    private static final String requestUrl = "https://api.giphy.com/v1/gifs/search" +
+            "?api_key=" + System.getenv().get("GIPHY_API_KEY") +
+            "&q=" + queryWelcomeInRussian +
+            "&limit=1&rating=G&lang=ru&offset=";
 
     protected HelloBot(String botToken, String botUsername, DefaultBotOptions botOptions) {
         super(botToken, botUsername, botOptions);
